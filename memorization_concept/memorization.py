4eb056a15091624093d31d97faf4496cafe359d2
jerrylei98 <jerrylei98@gmail.com>
2016-02-09 13:52:53 -0500

    concept works. android dev next

--- memorization_concept/memorization.py
+++ memorization_concept/memorization.py
@@ -10,7 +10,7 @@ def app(f_name):
     lines = fd.split('\n')
     c1 = 0
     while(c1 < len(lines)):
-        lines[c1] = re.sub('[^0-9a-zA-Z]+', '', lines[c1])
+        #lines[c1] = re.sub('[^0-9a-zA-Z]+', '', lines[c1])
         if len(lines[c1]) == 0:
             lines.pop(c1)
         c1+=1
@@ -21,9 +21,15 @@ def game():
     lines = app('hamlet.txt')
     current_line=0
     while current_line < len(lines):
-        if current_line > 0:
-            print "Previous line:"
+        if current_line == 0:
+            guess = raw_input("First line: ")
+            if re.sub('[^0-9a-zA-Z]+','',guess) == re.sub('[^0-9a-zA-Z]+','',lines[current_line]):
+                current_line += 1
+        elif current_line > 0:
+            print "Previous line: " + lines[current_line-1]
+            guess = raw_input("Line: ")
+            if re.sub('[^0-9a-zA-Z]+','',guess) == re.sub('[^0-9a\-zA-Z]+','',lines[current_line]):                                                 current_line += 1
 
 
-
+game()
 
