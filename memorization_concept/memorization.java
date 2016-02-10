c3f830ec273883219bc395ba97d5e9a499c4663c
jerrylei98 <jerrylei98@gmail.com>
2016-02-10 14:45:35 -0500

    java fileread done

--- memorization_concept/memorization.java
+++ memorization_concept/memorization.java
@@ -1,6 +1,29 @@
 //convert to java from python
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+
 class memorization{
+    private static ArrayList<String> read_lines(String fname){
+	ArrayList<String> return_string = new ArrayList<String>();
+	try{
+	    BufferedReader br = new BufferedReader(new FileReader(fname));
+	    String line = null;
+	    while((line = br.readLine()) != null){
+		//System.out.println(line);
+		return_string.add(line);
+	    }
+	    br.close();
+	}
+	catch(Exception e){
+	    e.printStackTrace();
+	}
+	return return_string;
+    }
+
     public static void main(String[] args){
-	System.out.println("hello world");
+	//System.out.println("hello world");
+	ArrayList<String> temp = read_lines("hamlet.txt");
+	System.out.println(temp);
     }
 }
