a96e0e7e0b1976e9caba297817442337777d09de
jerrylei98 <jerrylei98@gmail.com>
2016-02-11 10:38:48 -0500

    update java file

--- memorization_concept/memorization.java
+++ memorization_concept/memorization.java
@@ -2,8 +2,10 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 class memorization{
+    //Reads in file and separates into ArrayList by line
     private static ArrayList<String> read_lines(String fname){
 	ArrayList<String> return_string = new ArrayList<String>();
 	try{
@@ -20,7 +22,22 @@ class memorization{
 	}
 	return return_string;
     }
-
+    private String check(String guess, String accept){
+	
+    }
+    private void game(String fname){
+	ArrayList<String> lines = read_lines(fname);
+	int line_counter = 0;
+	Scanner in = new Scanner(System.in);
+	while(line_counter <= lines.size()){
+	    if(line_counter == 0){
+		guess = in.next();
+		if(guess.replaceAll('[^a-zA-Z0-9]', "") == 
+	    }
+	    else{
+	     
+	    }
+    }	
     public static void main(String[] args){
 	//System.out.println("hello world");
 	ArrayList<String> temp = read_lines("hamlet.txt");
